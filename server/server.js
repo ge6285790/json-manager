92e6e41ee32c77842165feb6069acfa8320c1fa5
Kyle Cheng <ge6285790@gmail.com>
2017-02-13 01:44:30 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,47 @@
-import express from 'express';
-import webpack from 'webpack';
-import WebpackDevMiddleware from 'webpack-dev-middleware';
-import WebpackHotMiddleware from 'webpack-hot-middleware';
-import cool from 'cool-ascii-faces';
-import config from '../webpack.dev.config';
+// import express from 'express';
+// import webpack from 'webpack';
+// import WebpackDevMiddleware from 'webpack-dev-middleware';
+// import WebpackHotMiddleware from 'webpack-hot-middleware';
+// import cool from 'cool-ascii-faces';
+// import config from '../webpack.dev.config';
+//
+//
+// const compiler = webpack(config);
+// const app = express();
+//
+// const PORT = process.env.PORT || 8000;
+//
+// app.use(WebpackDevMiddleware(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath,
+//   stats: { color: true },
+// }));
+//
+// app.use(WebpackHotMiddleware(compiler));
+//
+// app.use('/assets', express.static(__dirname + '/../public'));
+//
+// app.get('/cool', (req, res) => {
+//   // console.log('aaaa');
+//   res.send(cool());
+// });
+//
+// app.listen(PORT, () => {
+//   console.log(`Listening on ${PORT}`);
+// });
+
+var express = require('express');
+var webpack = require('webpack');
+var WebpackDevMiddleware = require('webpack-dev-middleware');
+var WebpackHotMiddleware = require('webpack-hot-middleware');
+var cool = require('cool-ascii-faces');
+var config = require('../webpack.dev.config');
 
 
-const compiler = webpack(config);
-const app = express();
+var compiler = webpack(config);
+var app = express();
 
-const PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8000;
 
 app.use(WebpackDevMiddleware(compiler, {
   noInfo: true,
